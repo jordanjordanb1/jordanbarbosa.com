b4639383813400cfca8c37a6553dbe0cc9c11d2e
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-27 23:55:22 -0500

    Turned into dumb component

--- client/src/components/Home/ViewCode/ViewCodeComponent.js
+++ client/src/components/Home/ViewCode/ViewCodeComponent.js
@@ -1,16 +1,14 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 
 import './background.png'
 import './ViewCodeComponent.css'
 
-export default class ViewCodeComponent extends PureComponent {
-    render() {
-        return (
-            <div className="view-code">
-                <a href="https://github.com/jordanjordanb1/jordanjordanb1.github.io" className="view-code-link" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <div className="view-code-triangle"></div>
-                <i className="fas fa-code view-code-text"></i>
-            </div>
-        )
-    }
+export default function ViewCode() {
+    return (
+        <div className="view-code">
+            <a href="https://github.com/jordanjordanb1/jordanjordanb1.github.io" className="view-code-link" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <div className="view-code-triangle"></div>
+            <i className="fas fa-code view-code-text"></i>
+        </div>
+    )
 }
