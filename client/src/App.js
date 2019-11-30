7eb8e008c1f8fefebdc10d5f564509e180420166
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-11-30 12:17:33 -0500

    Setup conditional store depending on dev mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
 import React, { PureComponent } from 'react';
 import { Provider } from  'react-redux'
 import { ConfigureStore } from './redux/configureStore'
-import { ConfigureDevStore } from './redux/configureDevStore'
-import { isProd } from './config'
 
 import './App.css';
 
 import Main from './components/MainComponent'
 
 // Configures the redux store
-let store;
-
-if (isProd) {
-    store = ConfigureStore()
-} else {
-    store = ConfigureDevStore()
-}
+const store = ConfigureStore()
 
 export default class App extends PureComponent {
     render() {
--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,12 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import { isProd } from '../config'  
 
 import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 import { contact } from './contact'
 import { console } from './console'
 import { projects } from './projects'
 import { login } from './login'
+
+let middleware = []
+
+if (isProd) {
+    middleware = [...middleware, thunk]
+} else {
+    middleware = [...middleware, thunk, logger]
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -17,7 +27,7 @@ export const ConfigureStore = () => {
             login,
             form: formReducer
         }),
-        applyMiddleware(thunk)
+        applyMiddleware(...middleware)
     )
 
     return store
